520aa0fcd2e47c7ac2475efe07e13624180254a7
Harald Stiff <hstiff@kth.se>
2018-05-07 17:47:19 +0200

    With distance and training with one digit.

--- RATTFIL.py
+++ RATTFIL.py
@@ -30,7 +30,6 @@ class Traindata:
 
         f_ones = np.ones([fake_images.shape[0],1])
         f_zeros = np.zeros([fake_images.shape[0],1])
-
         # Create lables for real and fake images
         self.real_lables = np.hstack((r_ones,r_zeros))
         self.fake_lables = np.hstack((f_zeros,f_ones))
@@ -38,7 +37,7 @@ class Traindata:
         self.lables = np.vstack((self.real_lables[:trainsize],self.fake_lables[:trainsize]))
         self.combineimages()
         self.shuffledata()
-    
+
     def combineimages(self):
         # Make the combined image vector equal part real and fake
         trainsize = min(self.fake_lables.shape[0],self.real_lables.shape[0])
@@ -52,7 +51,7 @@ class Traindata:
         np.random.shuffle(rand_vec)
         self.lables_shuf = self.lables[rand_vec]
         self.images_shuf = self.images[rand_vec]
-    
+
     def shufflenewdata(self,fake_images):
         # Update the shuffle images from new faked images
         self.fake_images = fake_images
@@ -63,15 +62,36 @@ class Traindata:
         self.images_shuf = self.images[rand_vec]
 
 
-def getdata():
+def getdata(digit=1):
     """Hämta bilderna"""
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-    images=mnist.train.images.astype('float32')
+    if digit==None:
+        images=mnist.train.images.astype('float32')
+    else:
+        images=mnist.train.images.astype('float32')
+        labels=mnist.train.labels
+        col=labels[:,digit]
+        col=col!=0
+        images=images[col]
+
+    #images=ims
+    print(images.shape)
+
+
     images=images.reshape(images.shape[0],28,28,1)
     images=np.pad(images,((0,0),(2,2),(2,2),(0,0)),'constant')
     images=2*(images-0.5)
+    plt.imshow(images[3,:,:,0])
+    plt.show()
+
+    plt.imshow(images[444,:,:,0])
+    plt.show()
     #print(images.shape)
     return images
+
+
+
+
 
 def getnoise(size):
     """Generera brus till generatorn"""
@@ -98,13 +118,13 @@ def generator():
 """ NY DISKRIMINATOR"""
 def discriminator():
     model=Sequential()
-    model.add(Conv2D(128,(5,5),strides=(2,2),input_shape=(32,32,1),padding='same'))
+    model.add(Conv2D(128//2,(5,5),strides=(2,2),input_shape=(32,32,1),padding='same'))
     model.add(LeakyReLU(0.2))
     model.add(BatchNormalization())
-    model.add(Conv2D(256,(5,5),strides=(2,2),padding='same'))
+    model.add(Conv2D(256//2,(5,5),strides=(2,2),padding='same'))
     model.add(LeakyReLU(0.2))
     model.add(BatchNormalization())
-    model.add(Conv2D(512,(5,5),strides=(2,2),padding='same'))
+    model.add(Conv2D(512//2,(5,5),strides=(2,2),padding='same'))
     model.add(LeakyReLU(0.2))
     model.add(BatchNormalization())
     model.add(Conv2D(1,(4,4),strides=(2,2),padding='valid',activation='sigmoid'))
@@ -135,7 +155,7 @@ def creatediscriminator():
     model.add(Dense(256,activation='relu'))
     model.add(Dense(2,activation='softmax'))
     return model
-    
+
 
 def settrainable(discmodel,Boolean):
     if Boolean == True:
@@ -163,19 +183,30 @@ def shuffle(batch_size,nr_images=55000):
     random_vec=np.arange(nr_images)
     return random_vec[0:batch_size]
 
-def showim(genmodel):
+def showim(genmodel,index):
     noise = getnoise(1)
     generated = genmodel.predict(noise)
+    filename="im"+str(index)+".png"
     plt.imshow(generated.reshape([32,32]),cmap='gray')
-    plt.show()
+    plt.savefig(filename)
+    #plt.show()
 
 
 #def train():
-images = getdata()
+images = getdata(1)
 discmodel = discriminator()
 genmodel = generator()
 
-l=50
+
+
+def dist(truemat,falsemat):
+    return np.linalg.norm(truemat-falsemat)
+
+
+
+
+
+l=100
 for i in range(200):
     noise = getnoise(l//2)
     noise_images =  genmodel.predict(noise)
@@ -188,14 +219,18 @@ for i in range(200):
     discmodel.fit(images[i*l//2:(i+1)*l//2,:,:,:],np.ones([l//2,1]),batch_size=l//2,epochs=1,verbose=2)
     discmodel.fit(noise_images,np.zeros([l//2,1]),batch_size=l//2,epochs=1,verbose=2)
     settrainable(discmodel,False)
-    
+
     noise = getnoise(l)
     gansmodel = creategans(discmodel,genmodel)
     print("Generator")
     gansmodel.fit(noise, np.ones([l,1]),batch_size=l,epochs=1,verbose=2)
+    distance=dist(images[0],noise_images[0])
+    print("Distance:")
+    print(distance)
 
-    if (i%15==0):
-        showim(genmodel)
+    if (i%5==0):
+        print(i)
+        showim(genmodel,i//5)
 
 for i in range(40):
     noise = getnoise(50)
@@ -206,15 +241,15 @@ for i in range(40):
 
     discmodel.fit(traindata.images_shuf,traindata.lables_shuf[:,0],batch_size=100,epochs=1,verbose=2)
     settrainable(discmodel,False)
-    
+
     noise = getnoise(100)
     gansmodel = creategans(discmodel,genmodel)
     gansmodel.fit(noise, np.ones([100,1]),batch_size=100,epochs=1,verbose=2)
 
 
 
-    
-    
+
+
 #train()
 
 
@@ -232,4 +267,3 @@ for i in range(40):
 #fake2=disc.predict(fake)
 ##fake2=disc.predict(fake)
 #print(fake2)
-
